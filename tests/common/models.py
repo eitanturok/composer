9d19416d48fcfcea204a78c5c51519765fc5bdeb
Eitan Turok <eitan.turok@gmail.com>
2024-08-23 23:07:37 +0000

    layer plan needs to access layers fc1, fc2

--- tests/common/models.py
+++ tests/common/models.py
@@ -141,6 +141,8 @@ class SimpleComposerMLP(ComposerClassifier):
 
         net = torch.nn.Sequential(fc1, torch.nn.ReLU(), fc2)
         super().__init__(num_classes=num_classes, module=net)
+        self.fc1 = fc1
+        self.fc2 = fc2
 
 
 class SimpleWeightTiedModel(ComposerClassifier):
