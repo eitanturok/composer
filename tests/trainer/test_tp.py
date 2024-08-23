e4083e4bb2c75f05cb007e1f64ab94c01c4bf767
Eitan Turok <eitan.turok@gmail.com>
2024-08-23 23:07:44 +0000

    update

--- tests/trainer/test_tp.py
+++ tests/trainer/test_tp.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader
 
 from composer.trainer.trainer import Trainer
 from composer.utils import dist
+from composer.callbacks import MemoryMonitor
+from composer.loggers import InMemoryLogger
 from tests.common import (
     RandomClassificationDataset,
     SimpleComposerMLP,
@@ -129,10 +131,12 @@ def test_tp_with_subset_of_params(world_size: int):
 @pytest.mark.skipif(version.parse(torch.__version__) < version.parse('2.3'), reason='requires PyTorch 2.3+')
 @pytest.mark.filterwarnings(r'ignore:.*\(TP\) is experimental.*:FutureWarning')
 def test_tp_correctness(world_size: int):
+    from torch.distributed.tensor.parallel import ColwiseParallel, RowwiseParallel
     from icecream import ic
 
+    SEED = 42
     HIDDEN_DIM = 2048 # the number of features in a single example
-    OUTPUT_DIM = 10 # the number of classes
+    OUTPUT_DIM = 10 # the number of classes in a single example
     BATCH_DIM = 2 # number of examples in a single batch on a single GPU
     NUM_SAMPLES = 32 # the size of the entire dataset
 
@@ -142,14 +146,44 @@ def test_tp_correctness(world_size: int):
         dataloader = DataLoader(dataset, batch_size=batch_size, sampler=dist.get_sampler(dataset))
         return model, dataloader
 
+    # forward pass with no FSDP and no TP (DDP is done by default)
     model, dataloader = _helper(hidden_dim=HIDDEN_DIM, output_dim=OUTPUT_DIM, batch_size=BATCH_DIM, num_samples=NUM_SAMPLES)
-    trainer = Trainer(model=model)
-    outputs = trainer.predict(dataloader)
-    ic(outputs)
+    trainer = Trainer(
+        seed=SEED,
+        model=model
+        # callbacks=[MemoryMonitor()],
+        # loggers=[InMemoryLogger()],
+        )
+    outputs = torch.stack(trainer.predict(dataloader))
 
-    # model, dataloader = _helper()
-    # trainer = Trainer(model=model)
-    # outputs = trainer.predict(dataloader)
-    # ic(outputs)
+    # forward pass with FSDP and no TP
+    model_fsdp, dataloader_fsdp = _helper(hidden_dim=HIDDEN_DIM, output_dim=OUTPUT_DIM, batch_size=BATCH_DIM, num_samples=NUM_SAMPLES)
+    trainer_fsdp = Trainer(
+        seed=SEED,
+        model=model_fsdp,
+        parallelism_config={'fsdp': {}},
+        # callbacks=[MemoryMonitor()],
+        # loggers=[InMemoryLogger()],
+        )
+    outputs_fsdp = torch.stack(trainer_fsdp.predict(dataloader_fsdp))
 
+    # forward pass with FSDP and TP
+    layer_plan = {'fc1': ColwiseParallel(), 'fc2': RowwiseParallel()}
+    tp_config = {'layer_plan': layer_plan, 'tensor_parallel_degree': 2},
+    model_fsdp_tp, dataloader_fsdp_tp = _helper(hidden_dim=HIDDEN_DIM, output_dim=OUTPUT_DIM, batch_size=BATCH_DIM, num_samples=NUM_SAMPLES)
+    trainer_fsdp_tp = Trainer(
+        seed=SEED,
+        model=model_fsdp_tp,
+        # callbacks=[MemoryMonitor()],
+        # loggers=[InMemoryLogger()],
+        parallelism_config={'fsdp': {}, 'tp': tp_config},
+        )
+    outputs_fsdp_tp = torch.stack(trainer_fsdp_tp.predict(dataloader_fsdp_tp))
 
+    # match shape
+    assert torch.all(outputs.shape == outputs_fsdp.shape)
+    assert torch.all(outputs.shape == outputs_fsdp_tp.shape)
+
+    # match values
+    assert torch.allclose(outputs, outputs_fsdp)
+    assert torch.allclose(outputs, outputs_fsdp_tp)
