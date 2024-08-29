973bb0b198ce10fb17ad45df517e0dbd1b952a7c
Eitan Turok <eitan.turok@gmail.com>
2024-08-29 02:11:44 +0000

    better tests

--- tests/trainer/test_tp.py
+++ tests/trainer/test_tp.py
@@ -20,31 +20,64 @@ from tests.common import (
 
 
 @pytest.mark.gpu
-@world_size(4)
+@world_size(2)
 @pytest.mark.skipif(version.parse(torch.__version__) < version.parse('2.3'), reason='requires PyTorch 2.3+')
 @pytest.mark.filterwarnings(r'ignore:.*\(TP\) is experimental.*:FutureWarning')
 def test_tp_train(world_size: int):
+    from torch.distributed.tensor.parallel import ColwiseParallel, RowwiseParallel
+
+    model = SimpleModel().to("cuda")
+    dataset = RandomClassificationDataset(size=8)
+    dataloader = DataLoader(dataset, batch_size=2, sampler=dist.get_sampler(dataset))
+
+    layer_plan = {'fc1': ColwiseParallel(), 'fc2': RowwiseParallel()}
+    tp_config = {'layer_plan': layer_plan, 'tensor_parallel_degree': world_size}
+
+    # init 1
+    model.forward(dataloader[0])
+
+    # init 2
+    model.param_init_fn(model.fc1)
+    model.param_init_fn(model.fc2)
+
+    # init 3
+    for name, param in model.named_modules():
+        print(name)
+        print(param)
+        print()
+
+    model.fc1.weight = torch.nn.init.normal_(model.fc1.weight)
+    model.fc2.weight = torch.nn.init.normal_(model.fc2.weight)
+    model.fc1.bias = torch.nn.init.normal_(model.fc1.bias)
+    model.fc2.bias = torch.nn.init.normal_(model.fc2.bias)
+
+    trainer = Trainer(
+        model=model,
+        train_dataloader=dataloader,
+        parallelism_config={'tp': tp_config},
+        max_duration='3ba',
+    )
+
+    trainer.fit()
+
+@pytest.mark.gpu
+@world_size(4)
+@pytest.mark.skipif(version.parse(torch.__version__) < version.parse('2.3'), reason='requires PyTorch 2.3+')
+@pytest.mark.filterwarnings(r'ignore:.*\(TP\) is experimental.*:FutureWarning')
+def test_tp_fsdp_train(world_size: int):
     from torch.distributed.tensor.parallel import ColwiseParallel, RowwiseParallel
 
     model = SimpleModel()
     dataset = RandomClassificationDataset(size=8)
     dataloader = DataLoader(dataset, batch_size=2, sampler=dist.get_sampler(dataset))
 
-    layer_plan = {
-        'fc1': ColwiseParallel(),
-        'fc2': RowwiseParallel(),
-    }
+    layer_plan = {'fc1': ColwiseParallel(), 'fc2': RowwiseParallel()}
+    tp_config = {'layer_plan': layer_plan, 'tensor_parallel_degree': 2}
 
     trainer = Trainer(
         model=model,
         train_dataloader=dataloader,
-        parallelism_config={
-            'tp': {
-                'layer_plan': layer_plan,
-                'tensor_parallel_degree': 2,
-            },
-            'fsdp': {},
-        },
+        parallelism_config={'tp': tp_config, 'fsdp': {}},
         max_duration='3ba',
     )
 
@@ -124,3 +157,4 @@ def test_tp_with_subset_of_params(world_size: int):
             },
             max_duration='3ba',
         )
+
