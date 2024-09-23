40592387df1a7c98b37b25bcb0d24d864e1b92b7
Eitan Turok <eitan.turok@gmail.com>
2024-09-23 14:03:20 +0000

    update

---
+++ TEST2.py
@@ -0,0 +1,99 @@
+import torch
+from icecream import ic
+from torch.distributed._tensor import Shard, Replicate
+from torch.distributed.tensor.parallel import RowwiseParallel, ColwiseParallel
+from torch.utils.data import DataLoader
+
+from composer.callbacks import MemoryMonitor
+from composer.loggers import InMemoryLogger
+from composer.trainer.trainer import Trainer
+from tests.common import RandomClassificationDataset, SimpleComposerMLP
+from composer.utils import FSDPConfig, ParallelismConfig, TPConfig, dist, reproducibility
+from composer.devices import DeviceGPU
+
+
+##########################################################################################
+# Parameters
+##########################################################################################
+
+size: int = 4
+batch_size: int = 1
+num_classes: int = 2
+num_features: int = 6
+seed: int = 42
+device: torch.device = torch.device('cuda')
+
+
+##########################################################################################
+# Setup
+##########################################################################################
+
+reproducibility.seed_all(seed)
+
+# if not dist.is_initialized():
+#     ic('heyo')
+#     dist.initialize_dist(DeviceGPU(), timeout=300.0)
+# dist.barrier()
+
+
+##########################################################################################
+# DataLoader
+##########################################################################################
+
+dataset = RandomClassificationDataset(
+    shape=(num_features,),
+    num_classes=num_classes,
+    size=size,
+    device=device,
+)  # X=(num_features,), y=(,), i.e. scalar
+dataloader = DataLoader(
+    dataset,
+    sampler=dist.get_sampler(dataset),
+    batch_size=batch_size,
+)  # X=(batch_size, num_features), y=(batch_size,)
+
+
+##########################################################################################
+# Model
+##########################################################################################
+
+
+model = SimpleComposerMLP(num_features=num_features, device=device, num_classes=num_classes)
+
+
+##########################################################################################
+# Parallelism Config
+##########################################################################################
+
+fsdp_config = FSDPConfig(
+    state_dict_type='full',
+    sharding_strategy='SHARD_GRAD_OP',
+    mixed_precision='full',
+    use_orig_params=True,
+)
+layer_plan = {
+    'fc1': ColwiseParallel(),
+    'fc2': RowwiseParallel(),
+}
+
+tp_config = TPConfig(layer_plan=layer_plan, tensor_parallel_degree=2)
+parallelism_config = ParallelismConfig(fsdp=fsdp_config, tp=tp_config)
+
+
+##########################################################################################
+# Trainer
+##########################################################################################
+
+trainer = Trainer(
+    seed=seed,
+    device='gpu',
+    model=model,
+    max_duration='1ep',
+    train_dataloader=dataloader,
+    precision='fp32',
+    parallelism_config=parallelism_config,
+    callbacks=[MemoryMonitor()],
+    loggers=[InMemoryLogger()],
+    progress_bar=False,
+    log_to_console=False,
+)
--- tests/trainer/test_tp.py
+++ tests/trainer/test_tp.py
@@ -559,3 +559,120 @@ def test_tp_fsdp_state_dict(world_size: int):
         tp_fsdp_state_dict2 = tp_fsdp_trainer.state.state_dict()  # fails always
 
         compare_modules(tp_fsdp_state_dict1['model'], tp_fsdp_state_dict2['model'])
+
+
+
+
+def get_tp_fsdp_trainer2(
+    size: int = 4,
+    batch_size: int = 1,
+    num_classes: int = 2,
+    num_features: int = 2,
+    seed: int = 44,
+    device: Union[torch.device, str] = 'cpu',
+    replication: int = 0,
+    output_dir: str = '/my-tmp/',
+):
+    from shutil import rmtree
+    from streaming import MDSWriter, StreamingDataset
+    from icecream import ic
+    from torch.distributed.tensor.parallel import ColwiseParallel, RowwiseParallel
+
+    fsdp_config = FSDPConfig(
+        state_dict_type='full',
+        sharding_strategy='SHARD_GRAD_OP',
+        mixed_precision='full',
+        use_orig_params=True,
+    )
+
+    layer_plan = {
+        'fc1': ColwiseParallel(),
+        'fc2': RowwiseParallel(),
+    }
+
+    tp_config = TPConfig(
+        layer_plan=layer_plan,
+        tensor_parallel_degree=replication,
+    )
+    parallelism_config = ParallelismConfig(fsdp=fsdp_config, tp=tp_config)
+
+    reproducibility.seed_all(seed)
+    if isinstance(device, str):
+        device = torch.device(device)
+    ic(device)
+
+    model = SimpleComposerMLP(num_features=num_features, device=device, num_classes=num_classes)
+
+    # dataset: Dataset = RandomClassificationDatasetReplicated(
+    #     shape=(num_features,),
+    #     num_classes=num_classes,
+    #     size=size,
+    #     device=device,
+    #     replication=replication,
+    # )  # X=(num_features,), y=(,), i.e. scalar
+
+    # dataloader = DataLoader(
+    #     dataset,
+    #     sampler=dist.get_sampler(dataset),
+    #     batch_size=batch_size,
+    # )  # X=(batch_size, num_features), y=(batch_size,)
+
+
+
+    pytorch_dataset = RandomClassificationDataset(
+        shape=(num_features,),
+        num_classes=num_classes,
+        size=size,
+        device=device,
+    )
+
+    # clean directory
+    rmtree(output_dir)
+
+    # columns = {'x': 'ndarray:float32:2', 'y': 'int64'} # 2 -> features
+    columns = {'x': 'pkl', 'y': 'int64'}
+    with MDSWriter(out=output_dir, columns=columns) as out:
+        for i in range(len(pytorch_dataset)):
+            x, y = pytorch_dataset[i]
+            # out.write({'x': x.cpu().detach().numpy(), 'y': y.cpu().detach().numpy()})
+            out.write({'x': x.numpy(), 'y': y.numpy()})
+
+    streaming_dataset = StreamingDataset(
+        local=output_dir,
+        replication=replication,
+        batch_size=batch_size,
+        allow_unsafe_types=True
+    )
+
+    for i in range(streaming_dataset.num_samples):
+        x, y = streaming_dataset[i]
+        ic(x)
+        ic(y)
+        ic('\n')
+
+    dataloader = DataLoader(streaming_dataset)
+
+
+    tp_fsdp_trainer = Trainer(
+        seed=seed,
+        device='gpu',
+        model=model,
+        max_duration='1ep',
+        train_dataloader=dataloader,
+        precision='fp32',
+        parallelism_config=parallelism_config,
+        callbacks=[MemoryMonitor()],
+        loggers=[InMemoryLogger()],
+        progress_bar=False,
+        log_to_console=False,
+    )
+
+    return tp_fsdp_trainer
+
+
+if __name__ == '__main__':
+
+    replication = 2
+    ddp_trainer = get_ddp_trainer(replication=replication)
+    tp_fsdp_trainer = get_tp_fsdp_trainer2(replication=replication)
+    tp_fsdp_trainer.fit()
